9d97aaaa8bba72c77683fddcd262ded487d2e516
cliff barnes <cliffbarnes@live.com>
2013-08-07 06:06:44 -0400

    bring in the mongoose

--- app.js
+++ app.js
@@ -36,6 +36,37 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/games', function(req, res){
+  var mongoose = require ("mongoose");
+
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.  
+var uristring = 
+  process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/gamelist';
+
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+GameListProvider = mongoose.connect(uristring, function (err, res) {
+  if (err)GameListProvider =  { 
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+
+GameListProvider.prototype.schema = new mongoose.Schema({
+  
+    homeTeam: { type: String, trim: true },
+    awayTeam: { type: String, trim: true },
+    homeLine: { type: Number, min: 0},
+    awayLine: {type: Number, min: 0}
+});
+
+//create our data model
+var gamelistModel = mongoose.model('gamelist', GameListProvider.schema);
 
 	res.render('index',{
 		title: 'Enter Games into Week' 
