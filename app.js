da11ac5a26ad2bdbb7cbc4da9fc31f561d2c26a7
cliff barnes <cliffbarnes@live.com>
2013-08-07 05:36:27 -0400

    Initial Commit

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-//Mongo on Heroku Setup
-var mongo = require('mongodb');
-
-var mongoUri = process.env.MONGOLAB_URI || 
-  process.env.MONGOHQ_URL || 
-  'mongodb://localhost/mydb'; 
-
-mongo.Db.connect(mongoUri, function (err, db) {
-  db.collection('mydocs', function(er, collection) {
-    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
-    });
-  });
-});
-
 //module dependencies
 var express = require('express')
   , routes = require('./routes')
