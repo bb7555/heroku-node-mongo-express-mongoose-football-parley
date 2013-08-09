001f25f3511f56a1762a0347a35398402cb2cc60
cliff barnes <cliffbarnes@live.com>
2013-08-09 06:48:29 -0400

    test game save crud

--- controllers/gamelist.js
+++ controllers/gamelist.js
@@ -50,10 +50,5 @@ exports.delete = function(req, res){
 }
 
 exports.sort = function(req, res){
-	Gamelist
-		.find()
-		.where(week_id).equals(req.query.week_id)
-		.exec(function(err, games){
-			
-		});
+	
 }
