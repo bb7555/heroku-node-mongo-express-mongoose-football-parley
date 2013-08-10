ef10884501e7967d366b138909ef4b543333b85c
cliff barnes <cliffbarnes@live.com>
2013-08-10 19:04:31 -0400

    fix typo

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-var express = require('express'),
-    passport = require('passport'),
-    TwitterStrategy = require('passport-twitter').Strategy,
-    ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn,
-    app = express();
- 
+//module dependencies
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path')
+  , mongoose = require('mongoose')
+  , passport = require('passport')
+  , TwitterStrategy = require('passport-twitter').Strategy
+  , ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
+
+//heroku mongo connection string
+  var uristring = 
+  process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/football-parley';
+
+var app = express();
+
+// connect to Mongo when the app initializes
+mongoose.connect(uristring);
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.cookieParser());
 app.use(express.session({ secret: 'keyboard cat' }));
 app.use(passport.initialize());
 app.use(passport.session());
- 
+
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
@@ -36,11 +60,28 @@ passport.use(new TwitterStrategy({
   }
 ));
  
- 
-app.get('/', function(req, res){
-    res.send('Hello World');
-  });
- 
+
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+//get app controllers
+var gamelist = require('./controllers/gamelist.js');
+
+//app routes
+
+app.get('/', routes.index);
+app.get('/games/index', gamelist.list);
+app.get('/games/create', gamelist.create);
+app.post('/games/create', gamelist.save);
+app.get('/games/update', gamelist.single);
+app.post('/games/update', gamelist.update);
+app.get('/games/delete', gamelist.delete);
+app.get('/games/byWeek', gamelist.sort);
+
+
 app.get('/account',
   ensureLoggedIn('/login'),
   function(req, res) {
@@ -60,9 +101,8 @@ app.get('/logout',
  
 app.get('/auth/twitter', passport.authenticate('twitter'));
 app.get('/auth/twitter/callback', passport.authenticate('twitter', { successReturnToOrRedirect: '/account', failureRedirect: '/login' }));
- 
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
