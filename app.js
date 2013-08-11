f6e32dd5e6c3e9b065726c1c59bb1469ef6da24f
cliff barnes <cliffbarnes@live.com>
2013-08-10 22:56:19 -0400

    working

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.get('/games/byWeek', gamelist.sort);
 app.get('/account',
   ensureLoggedIn('/login'),
   function(req, res) {
-    res.send('Hello ' + req.user.username + req.user.id + req.user.email);
+    res.send('Hello ' + req.user.username + req.user.id + req.user.emails.value);
   });
  
 app.get('/login',
