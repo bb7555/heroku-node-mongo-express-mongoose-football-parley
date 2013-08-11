e49bc0a82a61b39688ee3cb207ba656d3e7a9b39
cliff barnes <cliffbarnes@live.com>
2013-08-11 07:53:41 -0400

    working

--- models/ticket.js
+++ models/ticket.js
@@ -11,82 +11,82 @@ var ticketSchema = new mongoose.Schema({
     	game_id:{type:String, ref:'Gamelist'},
     	pick: String,
     	winner:Number
-    }
+    },
     game2: {
     	game_id:{type:String, ref:'Gamelist'},
     	pick: String,
     	winner:Number
-    }
+    },
     game3: {
     	game_id:{type:String, ref:'Gamelist'},
     	pick: String,
     	winner:Number
-    }
+    },
     game4: {
     	game_id:{type:String, ref:'Gamelist'},
     	pick: String,
     	winner:Number
-    }
+    },
     game5: {
     	game_id:{type:String, ref:'Gamelist'},
     	pick: String,
     	winner:Number
-    }
+    },
     game6: {
     	game_id:{type:String, ref:'Gamelist'},
     	pick: String,
     	winner:Number
-    }
+    },
     game7: {
     	game_id:{type:String, ref:'Gamelist'},
     	pick: String,
     	winner:Number
-    }
+    },
     game8: {
     	game_id:{type:String, ref:'Gamelist'},
     	pick: String,
     	winner:Number
-    }
+    },
     game9: {
     	game_id:{type:String, ref:'Gamelist'},
     	pick: String,
     	winner:Number
-    }
+    },
     game10: {
     	game_id:{type:String, ref:'Gamelist'},
     	pick: String,
     	winner:Number
-    }
+    },
     game11: {
     	game_id:{type:String, ref:'Gamelist'},
     	pick: String,
     	winner:Number
-    }
+    },
     game12: {
     	game_id:{type:String, ref:'Gamelist'},
     	pick: String,
     	winner:Number
-    }
+    },
     game13: {
     	game_id:{type:String, ref:'Gamelist'},
     	pick: String,
     	winner:Number
-    }
+    },
     game14: {
     	game_id:{type:String, ref:'Gamelist'},
     	pick: String,
     	winner:Number
-    }
+    },
     game15: {
     	game_id:{type:String, ref:'Gamelist'},
     	pick: String,
     	winner:Number
-    }
+    },
     game16: {
     	game_id:{type:String, ref:'Gamelist'},
     	pick: String,
     	winner:Number
-    }
+    },
     overUnder: Number
 });
  
